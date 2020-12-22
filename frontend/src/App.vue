1bba38690f3a52805c3e900b9a36d1519c5c24d5
LittleWhiteLoti <little.white.loti@gmail.com>
2020-12-22 02:12:37 -0500

    Styling & Needed additional component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -48,18 +48,67 @@ export default {
 }
 </script>
 
+<style>
+
+  html, body {
+    margin: 0;
+  }
+
+  body {
+    background-color: #d1fce0;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+</style>
 <style scoped>
 
   header {
-
+    background-color: #007700;
+    border-bottom: 3px solid #004400; 
+    height: 38px;
   }
 
   nav {
+    width: 1024px;
+    margin: 4px auto 12px auto;
+    display: flex;
+  }
 
+  nav a {
+    flex-grow: 1;
+    font-weight: bold;
+    background-color: #007700;
+    border-top: 1px solid #004400;
+    border-right: 1px solid #004400;
+    border-bottom: 1px solid #004400;
+    color: #FFFFFF;
+    text-align: center;
+    padding: 6px 0;
+  }
+  
+  nav a:first-child {
+    border-left: 1px solid #004400;
+    border-top-left-radius: 3px;
+    border-bottom-left-radius: 3px;   
+  }
+
+  nav a:last-child {
+    border-top-right-radius: 3px;
+    border-bottom-right-radius: 3px;
+  }
+
+  nav a {
+    text-decoration: none;
+  }
+
+  .router-link-active {
+    background-color: #005500;
+    color: #BBBBBB;
   }
 
   main {
-
+    width: 1024px;
+    margin: auto auto;
   }
 
 </style>
--- frontend/src/components/searches/ActiveSearches.vue
+++ frontend/src/components/searches/ActiveSearches.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        Active Searches
+        
     </div>
 </template>
 
