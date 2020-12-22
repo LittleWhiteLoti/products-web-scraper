437570a67351397ca90034b0cb3b8fc869c6de23
LittleWhiteLoti <little.white.loti@gmail.com>
2020-12-22 02:14:46 -0500

    Styling & Addition Item Link Component Needed

--- frontend/src/components/item/Item.vue
+++ frontend/src/components/item/Item.vue
@@ -1,11 +1,43 @@
 <template>
     <div>
-        Items
+        
     </div>
 </template>
 
 <script>
+
+
 export default {
-    
+    props: {
+        parentComponent: {
+            type: String,
+            required: true
+        },
+        image: {
+            type: String,
+            required: false
+        },
+        itemName: {
+            type: String,
+            required: true
+        },
+        lowestBuyPrice: {
+            type: Number,
+            required: true
+        },
+        highestBuyPrice: {
+            type: Number,
+            required: true
+        },
+        checkIntervals: {
+            type: String,
+            required: true
+        },
+        activelySearching: {
+            type: Boolean,
+            required: false
+        },
+        itemLinks: Array
+    }
 }
 </script>
